a3b883683cd23e8a2e2d6eab1677aa8ee5a4bf2d
Webovation <yanick@Yanicks-MacBook-Pro.local>
2016-05-09 11:53:10 +0200

    Layout files and the base of the main Stylesheet.

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php do_action('cornerstone_after_content'); ?>
+</div>
+
+<footer id="footer">
+	<div class="row">
+
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+<?php do_action('cornerstone_after_body'); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="initial-scale=1.0" />
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php do_action('cornerstone_before_body'); ?>
+
+<header id="header">
+	<div class="row">
+
+	</div>
+</header>
+
+<div id="content">
+<?php do_action('cornerstone_before_content'); ?>
